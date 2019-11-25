fa1afbf367e0e05a80af985c2510e86c94d3a49a
HiraRehman76 <56204287+HiraRehman76@users.noreply.github.com>
2019-11-25 18:42:09 +0500

    cofilicts

--- studentSittingArrangement/studentSittingArrangement/Program.cs
+++ studentSittingArrangement/studentSittingArrangement/Program.cs
@@ -15,6 +15,7 @@ namespace studentSittingArrangement
         static void Main()
         {
             Application.EnableVisualStyles();
+
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
